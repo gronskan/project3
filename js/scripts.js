02246a4c9b214a53a9fab85d93c4b7f920e48a87
jg <stratacast@comcast.net>
2019-06-22 17:28:46 -0700

    added function to convert array into unordered list

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,34 @@ $(document).ready(function() {
     event.preventDefault();
 
     var inputNumber = parseInt($("input#number").val());
-    var total = 0;
     var array = [];
-    for (var currentNumber = 1; currentNumber <= inputNumber; currentNumber += 1) {
-      array.push(currentNumber);
-    }
-console.log(array);
+    for (var index = 1; index <= inputNumber; index += 1) {
+      
+      array.push(index);
 
-    var result = total;
+    };
 
-    $("#answer").text(result);
+    $("#list").append("<li>" + (array.join("</li> <li>")) + "</li>");
+    (array.join("</li> <li>").val(""));
 
-  })
-})
+    $("#list").text(list);
+
+  });
+});
+
+
+
+  // $(document).ready(function() {
+  //   $("#pingpong").submit(function(event) {
+  //     event.preventDefault();
+  //
+  //     var inputNumber = parseInt($("input#number").val());
+  //     var array = [];
+  //     for (var index = 1; index <= inputNumber; index += 1) {
+  //       array.push(index);
+  //         var list = "<ul><li>" + array.join("</li><li>") + "</li></ul>";
+  //         console.log(list)
+  //       $("#list").text(list);
+  //       };
+  //     });
+  //   });

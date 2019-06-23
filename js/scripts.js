4e5943535f69e49cf0172e6f34790bb042c53bbe
jg <stratacast@comcast.net>
2019-06-22 18:40:17 -0700

    added function to replace numbers divisible by 3 to 'ping' numbers divisible by 5 to 'pong' and numbers divisible by both 3 and 5 to 'pingpong'

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,22 @@ $(document).ready(function() {
     var inputNumber = parseInt($("input#number").val());
     var array = [];
     for (var index = 1; index <= inputNumber; index += 1) {
-      
       array.push(index);
-
     };
 
-    $("#list").append("<li>" + (array.join("</li> <li>")) + "</li>");
-    (array.join("</li> <li>").val(""));
+    var array2 = []
+    array.forEach(function(arr) {
+      if (arr % 3 === 0 && arr % 5 ===0) {
+        array2.push("Ping Pong");
+      } else if (arr % 3 === 0) {
+        array2.push("Ping");
+      } else if (arr % 5 === 0) {
+        array2.push("Pong");
+      } else array2.push(arr);
+    });
+
+    $("#list").append("<li>" + (array2.join("</li> <li>")) + "</li>");
+    (array2.join("</li> <li>").val(""));
 
     $("#list").text(list);
 
